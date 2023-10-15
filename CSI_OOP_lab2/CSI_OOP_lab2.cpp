3c9b12466b8652dba594fb91b3a81248d1c335b1
E1 <ivanboitsov386@gmail.com>
2023-10-16 00:28:24 +0700

    Исправление ошибок в классе массива. Исправлена ошибка выделения памяти при добавлении нового элемента в конец массива или при формирования нового массива. Также исправлена ошибка выделении памяти при добавлении массива в конец другого массива. Добавлены тесты ко всем функция класса

--- CSI_OOP_lab2/CSI_OOP_lab2.cpp
+++ CSI_OOP_lab2/CSI_OOP_lab2.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 class Array {
@@ -64,7 +66,7 @@ public:
         n = m;
         mem = m;
         a = new int[n];
-        for (int i = 0; i < 0; i++) {
+        for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
     }
@@ -93,13 +95,55 @@ public:
             a[n] = key;
             n++;
         }
+        else {
+            // Выделяем новую память, например, удваиваем текущий размер массива
+            int newMem = mem * 2;
+            int* newArray = new int[newMem];
+
+            // Копируем существующие элементы в новый массив
+            for (int i = 0; i < n; i++) {
+                newArray[i] = a[i];
+            }
+
+            // Освобождаем старую память
+            delete[] a;
+
+            // Указываем на новый массив и обновляем память
+            a = newArray;
+            mem = newMem;
+
+            // Добавляем новый элемент
+            a[n] = key;
+            n++;
+        }
         return *this;
     }
     // Формирование массива b = a + key (key добавляется в конец)
-    Array& operator + (int key) {
-        Array b(*this);
-        if (n < mem) {
-            b.a[n] = key;
+    Array operator+(int key) {
+        Array b(*this); // Создаем копию массива a
+        if (b.n < b.mem) {
+            b.a[b.n] = key; // Добавляем элемент key в конец массива b
+            b.n++;
+        }
+        else {
+            // Выделяем новую память, например, удваиваем текущий размер массива
+            int newMem = b.mem * 2;
+            int* newArray = new int[newMem];
+
+            // Копируем существующие элементы из a в новый массив
+            for (int i = 0; i < b.n; i++) {
+                newArray[i] = b.a[i];
+            }
+
+            // Освобождаем старую память массива a
+            delete[] b.a;
+
+            // Указываем на новый массив и обновляем память и размер
+            b.a = newArray;
+            b.mem = newMem;
+
+            // Добавляем новый элемент key в конец массива b
+            b.a[b.n] = key;
             b.n++;
         }
         return b;
@@ -111,6 +155,29 @@ public:
                 a[n + i] = x.a[i];
             }
             n += x.n;
+        }
+        else {
+            // Плюсуем память двух массивов и создаем новый динамический
+            int newMem = mem + x.mem;
+            int* newArray = new int[newMem];
+
+            // Копируем существующие элементы из a в новый массив
+            for (int i = 0; i < n; i++) {
+                newArray[i] = a[i];
+            }
+            for (int i = 0; i < x.n; i++) {
+                newArray[n + i] = x.a[i];
+            }
+
+            // Освобождаем старую память массива a
+            delete[] a;
+
+            // Указываем на новый массив и обновляем память и размер
+            a = newArray;
+            mem = newMem;
+            
+            // Указываем новое количество элементов массива
+            n = n + x.n;
         }
         return *this;
     }
@@ -151,14 +218,26 @@ public:
         return *this;
     }
     // Формирование нового массива b, в котором удален элемент с позиции pos
-
     Array DelPosNew(int pos) {
-        Array b(*this);
-        if (pos >= 0 && pos < n) {
-            b.ShiftLeft(pos);
+        if (pos < 0 || pos >= n) {
+            // Если заданная позиция некорректна, просто вернуть копию текущего массива
+            return *this;
         }
+
+        Array b(n - 1); // Создаем новый массив b с размером на 1 элемент меньше
+
+        int j = 0; // Индекс для массива b
+
+        for (int i = 0; i < n; i++) {
+            if (i != pos) {
+                b.a[j] = a[i];
+                j++;
+            }
+        }
+
         return b;
     }
+
     // Проверка равенства массивов
     bool operator == (Array x) {
         if (n != x.n) {
@@ -213,10 +292,134 @@ public:
     //
     friend ostream& operator << (ostream& r, Array& x);
     friend istream& operator >> (istream& r, Array& x);
-
     };
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+
+    // Тест 1
+    Array mas1;
+    cout << "Введите числа массива через пробел: ";
+    mas1.Scan(5);
+    cout << "Элементы массива 'mas1': ";
+    mas1.Print();
+    cout << "\n";
+
+    // Тест 2
+    Array mas2(mas1);
+    cout << "Элементы массива 'mas2': ";
+    mas2.Print();
+    cout << "\n";
+
+    // Тест 3
+    if (mas1 == mas2) {
+        cout << "Массивы mas1 и mas2 равны" << "\n";
+    }
+    else {
+        cout << "Массивы mas1 и mas2 не равны" << "\n";
+    }
+
+    // Тест 4
+    mas1 += 6;
+    cout << "Элементы массива 'mas1': ";
+    mas1.Print();
+    cout << "\n";
+    if (mas1 == mas2) {
+        cout << "Массивы mas1 и mas2 равны" << "\n";
+    }
+    else {
+        cout << "Массивы mas1 и mas2 не равны" << "\n";
+    }
+    // Тест 5
+    Array mas3 = mas1 + 7;
+    cout << "Элементы массива 'mas3': ";
+    mas3.Print();
+    cout << "\n";
+
+    // Тест 6
+    mas3.DelPosEq(0);
+    cout << "Элементы массива 'mas3': ";
+    mas3.Print();
+    cout << "\n";
+
+    // Тест 7
+    Array mas4 = mas3.DelPosNew(5); 
+    cout << "Элементы массива 'mas4': ";
+    mas4.Print();
+    cout << "\n";
+
+    // Тест 8
+    mas1.DelPosEq(1);
+    if (mas1 == mas4) {
+        cout << "Массивы mas1 и mas4 равны" << "\n";
+    }
+    else {
+        cout << "Массивы mas1 и mas4 не равны" << "\n";
+    }
+
+    // Тест 9
+    
+    // Инициализируем генератор случайных чисел
+    srand(time(0));
+
+    int n = 10;
+    int* b = new int[n];
+    for (int i = 0; i < n; i++) {
+        b[i] = rand() % 100;
+    }
+    Array mas5(b, n);
+    /*cout << "Элементы массива 'mas5': ";
+    mas5.Print();
+    cout << "\n";*/
+
+    // Тест 10
+    cout << "Максимальный индекс = " << mas5.Max() << "; "
+        << "Максимальное число = " << mas5[mas5.Max()] << "\n";
+    cout << "Минимальный индекс = " << mas5.Min() << "; "
+        << "Минимальное число = " << mas5[mas5.Min()]  << "\n";
+
+    // Тест 11
+    mas5.Sorting();
+    cout << "Отсортированные элементы массива 'mas5': ";
+    mas5.Print();
+    cout << "\n";
+
+    // Тест 12
+    Array mas6 = mas1 + mas5;
+    cout << "Элементы массива 'mas6': ";
+    mas6.Print();
+    cout << "\n";
+
+    // Тест 13
+    Array mas7(4);
+    cout << "Введите числа массива через пробел: ";
+    mas7.Scan(4);
+
+    // Тест 14
+    if (mas7.FindKey(1) == -1) {
+        cout << "Данного элемента нет в массиве" << "\n";
+    }
+    else {
+        cout << "Индекс элемента в массиве = " << mas7.FindKey(1) << "\n";
+    }
+    if (mas7.FindKey(10) == -1) {
+        cout << "Данного элемента нет в массиве" << "\n";
+    }
+    else {
+        cout << "Индекс элемента в массиве = " << mas7.FindKey(10) << "\n";
+    }
+
+    // Тест 15
+    mas4 += mas7;
+    cout << "Элементы изменённого массива 'mas4' (+mas7): ";
+    mas4.Print();
+    cout << "\n";
+
+    // Тест 16
+    mas4 = mas6;
+    cout << "Присвоение mas4 = mas6: ";
+    mas4.Print();
+    cout << "\n";
     return 0;
 }
